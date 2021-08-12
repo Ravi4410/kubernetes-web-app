467b1033a31fd6c761b2321f34811d9b0eb1c259
Ravi4410 <85069242+Ravi4410@users.noreply.github.com>
2021-08-12 15:37:18 +0530

    Create listpod.py

---
+++ listpod.py
@@ -0,0 +1,10 @@
+import cgi
+import subprocess
+
+print("content-type:text/html")
+print()
+
+# List Pods
+print("\n\nList of all pods .....\n")
+list_pod = subprocess.getoutput("kubectl get pod --kubeconfig admin.conf")
+print(list_pod)
